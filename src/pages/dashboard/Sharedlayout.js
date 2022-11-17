4a5dfb15e0376b014daad2ef05917db8c142e3b0
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-17 11:17:29 +0100

    Complete SharedLayout

--- src/pages/dashboard/Sharedlayout.js
+++ src/pages/dashboard/Sharedlayout.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Navbar, SmallSidebar, BigSidebar } from '../../components';
+import Wrapper from '../../assets/wrappers/SharedLayout';
 
 const Sharedlayout = () => {
-  return <h1>Sharedlayout</h1>;
+  return (
+    <>
+      <Wrapper>
+        <main className='dashboard'>
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
 };
 
 export default Sharedlayout;
