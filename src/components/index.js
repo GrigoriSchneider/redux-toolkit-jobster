e157550c6c5090775183f1ded5691d459311e979
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-18 10:48:08 +0100

    Profile Page - intro

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import BigSidebar from './BigSidebar';
 import Logo from './Logo';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
+import FormRow from './FormRow';
 
-export { Logo, Navbar, SmallSidebar, BigSidebar };
+export { Logo, Navbar, SmallSidebar, BigSidebar, FormRow };
