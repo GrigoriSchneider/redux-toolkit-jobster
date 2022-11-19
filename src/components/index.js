d73c894089e02537d4c57d311096380864fed902
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-19 21:16:36 +0100

    container setup  => loading

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import FormRow from './FormRow';
 import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
+import Job from './Job';
 
-export { Logo, Navbar, SmallSidebar, BigSidebar, FormRow, FormRowSelect };
+export {
+  Logo,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRow,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  Job,
+};
