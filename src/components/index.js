6f8dfea70b3f3e9b993d481902220f4916bf45a6
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-19 16:51:53 +0100

    FormRowSelect

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Logo from './Logo';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
 
-export { Logo, Navbar, SmallSidebar, BigSidebar, FormRow };
+export { Logo, Navbar, SmallSidebar, BigSidebar, FormRow, FormRowSelect };
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormRow } from '../../components';
+import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
@@ -61,8 +61,20 @@ const AddJob = () => {
             handleChange={handleJobInput}
           />
           {/* job status */}
-
+          <FormRowSelect
+            name='status'
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
           {/* job type */}
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
 
           {/* btn container */}
           <div className='btn-container'>
